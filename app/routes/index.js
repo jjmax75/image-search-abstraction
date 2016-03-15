0cd6283ac6db7dabf9028b7921b3f73ff2ed1bca
John Behan <behanj@gmail.com>
2016-03-15 14:43:06 +0700

    cleaned up all initial files, base build

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,11 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+var path = process.cwd();
 
 module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
 
-   app.route('/')
-      .get(function (req, res) {
-         res.sendFile(process.cwd() + '/public/index.html');
-      });
-
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+	app.route('/')
+    .get(function(req, res) {
+      res.sendFile(path + '/public/index.html');
+    });
 };
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
+require('dotenv').config();
+var port = process.env.PORT || 8080;
+var path = process.cwd();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+mongo.connect(process.env.MONGO_URI, function(err, db) {
+	if (err) {
+		throw new Error('DB failed to connect');
+	} else {
+		console.log('Mongo DB successfuly connected on port 27017');
+	}
 
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
-   }
+	app.use('/controllers', express.static(path + '/app/controllers'));
+	app.use('/public', express.static(path + '/public'));
 
-   app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+	routes(app, db);
 
-   routes(app, db);
-
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
-   });
+	app.listen(port,  function () {
+		console.log('Node.js listening on port ' + port + '...');
+	});
 
 });
+
+module.exports = app;
